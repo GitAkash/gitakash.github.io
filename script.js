a65b64994c5efff3b968f0349437437a535b888f
Akash <akashmondalnl@gmail.com>
2022-03-09 20:56:10 +0000

    hh

--- script.js
+++
@@ -1,6 +0,0 @@
-let progress = "getElementById"('progressbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function(){
-    let progressHeight = (window.pageYoffset / totalHeight) * 100;
-    progress.style.height = progressHeight + "%";
-}
